7820bda2da3e7b35ad6f0b4f9680b3548ef299cc
Marcos-Reckers <marcosqlkr@hotmail.com>
2025-01-03 15:21:08 -0300

    a

--- src/serverClass.cpp
+++ src/serverClass.cpp
@@ -138,8 +138,10 @@ void Server::connect_server(string main_ip_address, string main_port)
     while (attempts < 10)
     {
         std::cout << "Dentro do while connect_server" << endl;
+        std::cout << "AQUI O PRINTEI PRINTA CARALHO" << endl;
         if (connect(curr_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == 0)
         {
+            std::cout << "AGR ESTOU DENTRO DO IF DO CONNECT" << endl;
             std::string username_with_null = this->backup_name;
             send(curr_sock, username_with_null.c_str(), username_with_null.size(), 0);
             int recebido = 0;
