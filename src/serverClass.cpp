314054e8dbd96b65e02064b08a8b87ae65329b45
Marcos-Reckers <marcosqlkr@hotmail.com>
2025-01-11 17:31:08 -0300

    sdfjsidisdsdf

--- src/serverClass.cpp
+++ src/serverClass.cpp
@@ -641,13 +641,23 @@ void Server::handle_io(int &client_sock, Threads::AtomicQueue<std::vector<Packet
 
             if (received_packet.get_type() == 1)
             {
-                if (received_packet.get_seqn() == received_packet.get_total_packets())
+                std::cout << "COMPARAÇÃO: " << packets_to_recv_queue.size() << " == " << received_packet.get_total_packets()-1 << std::endl;
+                if (received_packet.get_total_packets() == 1)
                 {
+                    std::cout << "sou um pacote solteiro e vou ser enviado" << std::endl;
                     packets_to_recv_queue.push_back(received_packet);
                     received_queue.produce(packets_to_recv_queue);
                     packets_to_recv_queue.clear();
                 }
-                else if (received_packet.get_seqn() < received_packet.get_total_packets())
+                else if (packets_to_recv_queue.size() == received_packet.get_total_packets()-1)
+                {
+                    packets_to_recv_queue.push_back(received_packet);
+
+                    sort_packets(packets_to_recv_queue);
+                    sync_queue.produce(packets_to_recv_queue);
+                    packets_to_recv_queue.clear();
+                }
+                else if (packets_to_recv_queue.size() < received_packet.get_total_packets()-1)
                 {
                     packets_to_recv_queue.push_back(received_packet);
                 }
@@ -670,7 +680,7 @@ void Server::handle_io(int &client_sock, Threads::AtomicQueue<std::vector<Packet
                     sync_queue.produce(packets_to_sync_queue);
                     packets_to_sync_queue.clear();
                 }
-                else if (packets_to_recv_queue.size() < received_packet.get_total_packets()-1)
+                else if (packets_to_sync_queue.size() < received_packet.get_total_packets()-1)
                 {
                     packets_to_sync_queue.push_back(received_packet);
                 }
